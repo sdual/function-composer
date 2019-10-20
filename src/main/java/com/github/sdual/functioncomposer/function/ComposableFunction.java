e6cae87b64990cecae0c53070d63aed23f0ac3d0
sdual <shr.nrht.0623@gmail.com>
2019-10-20 21:08:42 +0900

    add test of composed function

--- src/main/java/com/github/sdual/functioncomposer/function/ComposableFunction.java
+++ src/main/java/com/github/sdual/functioncomposer/function/ComposableFunction.java
@@ -2,19 +2,19 @@ package com.github.sdual.functioncomposer.function;
 
 import java.util.function.Function;
 
-public class ComposableFunction<T, R, S> implements Function<T, S> {
+public class ComposableFunction<S, T, R> implements Function<S, R> {
 
-  private final Function<R, S> nextFunc;
-  private final Function<T, R> func;
+  private final Function<S, T> func;
+  private final Function<T, R> nextFunc;
 
-  public ComposableFunction(Function<T, R> func, Function<R, S> nextFunc) {
+  private ComposableFunction(Function<S, T> func, Function<T, R> nextFunc) {
     this.func = func;
     this.nextFunc = nextFunc;
   }
 
   @SuppressWarnings("unchecked")
-  public ComposableFunction(Function<T, R> func) {
-    this(func, (Function<R, S>) Function.identity());
+  private ComposableFunction(Function<S, T> func) {
+    this(func, (Function<T, R>) Function.identity());
   }
 
   public static <T, R, S> ComposableFunction<T, R, S> from(Function<T, R> func) {
@@ -27,8 +27,8 @@ public class ComposableFunction<T, R, S> implements Function<T, S> {
   }
 
   @Override
-  public S apply(T t) {
-    return nextFunc.compose(func).apply(t);
+  public R apply(S s) {
+    return nextFunc.compose(func).apply(s);
   }
 
 }
--- src/test/java/com/github/sdual/functioncomposer/function/ComposableFunctionTest.java
+++ src/test/java/com/github/sdual/functioncomposer/function/ComposableFunctionTest.java
@@ -1,5 +1,33 @@
 package com.github.sdual.functioncomposer.function;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import org.junit.Test;
+
 public class ComposableFunctionTest {
+
+  private String addTestFunctionToString(int x) {
+    return String.valueOf(x + 10);
+  }
+
+  private List<String> stringToList(String str) {
+    return Arrays.asList(str.split(""));
+  }
+
+  @Test
+  public void testComposeFunctions() {
+    Function<Integer, List<String>> composedFunc = ComposableFunction
+        .from(this::addTestFunctionToString, ComposableFunction.from(this::stringToList));
+
+    List<String> actual = composedFunc.apply(10);
+    List<String> expected = Arrays.asList("2", "0");
+
+    assertThat("The Composed Function returns a List of split number string",
+        actual, is(expected));
+  }
 
 }
